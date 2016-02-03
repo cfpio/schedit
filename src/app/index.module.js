0b926a6c8454bd249887e04db02062f504825b71
Benjamin Plouzennec <benjamin.plouzennec@zenika.com>
2016-02-03 18:18:16 +0100

    improved a bit the UX in drag'n'drop

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('schedit', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.bootstrap', 'toastr']);
+    .module('schedit', ['ngDragDrop']);
 
 })();
