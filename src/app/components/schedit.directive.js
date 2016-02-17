656859a8f798a54285d7f95782b0ffef29d06768
Benjamin Plouzennec <benjamin.plouzennec@zenika.com>
2016-02-17 18:20:14 +0100

    V2 quasi en place

--- src/app/components/schedit.directive.js
+++ src/app/components/schedit.directive.js
@@ -7,13 +7,45 @@ angular.module('schedit').directive('scSchedit', function(){
     },
     bindToController:true,
     controllerAs:'schedit',
-    controller:function($scope, EventHelper){
+    controller:function($scope){
       var vm = this;
       $scope.$watch(function(){
         return vm.events;
       }, function(){
-        EventHelper.events = vm.events;
+        console.log(vm);
       })
+
+      vm.handleInput = function(){
+        if(vm.input[0] != 9){
+          var room = vm.findRoom(vm.input);
+          var day = vm.input[0];
+          vm.selectedSlot = vm.findSlot(vm.input, room);
+        }else{
+          console.log("This is a conf")
+        }
+        vm.input = "";
+      }
+
+      vm.findRoom = function(input){
+        var trackIndex = input[1];
+        return vm.options.rooms[trackIndex-1];
+      }
+
+      vm.findSlot = function(input, room){
+        var time = ""+input[2]+input[3]+":"+input[4]+input[5];
+        var day = room['day'+input[0]];
+        var found = false;
+        for(key in day){
+          console.log(day[key]);
+          angular.forEach(day[key],function(slot){
+            console.log(''+slot.start+' '+time);
+            if(slot.start == time){
+              found = slot;
+            }
+          })
+        }
+        return found;
+      }
     }
   }
 })
