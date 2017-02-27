d6fc2bed144f3ab54a48a912a0ba023b69d07c11
Alex Thomazo <alex@thomazo.info>
2017-02-27 16:36:37 +0100

    duration and colors

--- src/app/core/EventHelper.service.js
+++ src/app/core/EventHelper.service.js
@@ -3,35 +3,27 @@ angular.module('schedit').service('EventHelper', function(){
   var service = {};
   service.spaceGap = 15;
   service.spaceMapping = {
-    "Docker Docker Docker":45,
-    "Black Belt Tech":30,
-    "Ecosystem":60,
-    "Pre-Registration, Registration":60,
-    "Registration":45,
-    "Party":30,
-    "Wild Cards":30,
-    "Use Case":15,
-    "break":15,
+    "Conférence":55,
+    "Tool in Action":25,
+    "Université":120,
+    "Quickie":15,
+    "Lab":120,
     "default":30
   }
 
   service.colorMapping = {
-    "Docker Docker Docker":"#2F687B",
-    "Black Belt Tech":"#3E4089",
-    "Ecosystem":"#5E3685",
-    "Pre-Registration, Registration":"#9B3874",
-    "Registration":"#C55647",
-    "Party":"#C58C47",
-    "Wild Cards":"#C5A847",
-    "Use Case":"#C5C247",
-    "break":"#8BB642",
+    "Conférence":"#2F687B",
+    "Tool in Action":"#3E4089",
+    "Université":"#5E3685",
+    "Quickie":"#9B3874",
+    "Lab":"#C55647",
     "default":"#338C5A"
   }
 
   service.getNbSpaces = function(event){
     var result;
-    if(service.spaceMapping[event['event_type']]){
-      result = service.spaceMapping[event['event_type']]/service.spaceGap;
+    if(service.spaceMapping[event['format']]){
+      result = service.spaceMapping[event['format']]/service.spaceGap;
     }else{
       result = service.spaceMapping["default"]/service.spaceGap;
     }
@@ -40,8 +32,8 @@ angular.module('schedit').service('EventHelper', function(){
 
   service.getColor = function(event){
       var result;
-      if(service.colorMapping[event['event_type']]){
-        result = service.colorMapping[event['event_type']];
+      if(service.colorMapping[event['format']]){
+        result = service.colorMapping[event['format']];
       }else{
         result = service.colorMapping["default"];
       }
